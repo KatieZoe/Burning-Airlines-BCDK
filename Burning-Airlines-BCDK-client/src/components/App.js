9c7409def296102c1bfe71825e9a8040bc324a72
Fei Ye <feiye@Feis-MacBook-Pro.local>
2021-05-11 22:58:33 +1000

    create client and server folder to include both frontend and backend

---
+++ Burning-Airlines-BCDK-client/src/components/App.js
@@ -0,0 +1,16 @@
+
+import Homepage from './Homepage'
+import Nav from './Nav'
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Welcome from App.js</h1>
+      <Nav />
+      <Homepage />
+
+    </div>
+  );
+}
+
+export default App;
