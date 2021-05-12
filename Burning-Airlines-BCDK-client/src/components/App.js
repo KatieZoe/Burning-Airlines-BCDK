b6505fce0889dc7d1439c390238af8d0f2f644b3
Fei Ye <feiye@Feis-MacBook-Pro.local>
2021-05-12 10:15:30 +1000

    deleted flight detail page for now

--- Burning-Airlines-BCDK-client/src/components/App.js
+++ Burning-Airlines-BCDK-client/src/components/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Homepage from "./Homepage";
 import Airplanes from "./Airplanes";
 import Flights from "./Flights";
 import Search from "./Search";
-import FlightDetails from "./FlightDetails";
 import Nav from "./Nav";
 import Admin from "./Admin";
 
@@ -19,7 +18,6 @@ function App() {
           <Route exact path="/flights" component={Flights}/>;
           <Route exact path="/search" component={Search}/>;
           <Route exact path="/admin" component={Admin}/>;
-          <Route exact path="/flight/:flightID" component={FlightDetails}/>;
         </Switch>
       </div>
     </Router>
